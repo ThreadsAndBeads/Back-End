88353862a76d2bc3496c2bc40e4a2fcb1f30005b
marim99 <marim.khaled99@gmail.com>
2023-05-01 20:32:44 +0300

    facebookLogin

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require("express");
 const authRoutes = require("./Routes/UserRoutes");
 const productRoutes = require("./Routes/ProductRoutes");
 const app = express();
+const passport = require("passport");
 const dotenv = require("dotenv");
+const session = require("express-session");
 dotenv.config({ path: "./config.env" });
 app.use(express.json());
+require("./utils/facebook");
 const mongoose = require("mongoose");
 const DB_URL = process.env.DATABASE_URL;
 
@@ -16,6 +19,18 @@ mongoose
   .catch((err) => {
     console.error("Connection error:", err);
   });
+// signup with facebook
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(
+  session({
+    secret: "your-session-secret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.listen(process.env.PORT, () => {
   console.log("http://localhost:" + process.env.PORT);
