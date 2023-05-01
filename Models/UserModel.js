3b14717c3293072ef6d4b73584ee9f6cf8a899b7
radwa nabil <radwaismail444@gmail.com>
2023-05-01 13:04:13 +0300

    added error messages

--- Models/UserModel.js
+++ Models/UserModel.js
@@ -1,14 +1,17 @@
 const mongoose = require('mongoose');
+const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
-        required: true,
+        required: [true,'Please enter an email address'],
         unique: true,
+        validate: [isEmail,'Please enter a valid email address']
 
     }, password: {
         type: String,
-        required: true,
-        minLength: 6
+        required: [true,'Please enter a password'],
+        minlength: [6,'Minimum password length is 6 characters'],
     }, type: {
         type: String,
         required: true,
@@ -20,5 +23,11 @@ const userSchema = new mongoose.Schema({
     }
 })
 
+//fire function before user is saved to database
+userSchema.pre('save', async function (next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
 const User = mongoose.model('user', userSchema); 
 module.exports = User;
