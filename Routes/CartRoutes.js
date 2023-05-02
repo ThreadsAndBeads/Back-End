48016ee515a12baabef233e575df9b85d09381ae
Nada Saeed <nada1998saeed@gmail.com>
2023-05-02 21:42:34 +0300

    add to cart

---
+++ Routes/CartRoutes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const cartController = require("./../Controllers/CartController");
+const router = express.Router();
+
+router.route("/").post(cartController.AddToCart);
+
+module.exports = router;
