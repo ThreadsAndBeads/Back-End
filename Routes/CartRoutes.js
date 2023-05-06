03aeee5ab8407fe1ea44fc872a48eb9f65ef4033
orabi55555 <elorabiahmed843@gmail.com>
2023-05-06 14:37:06 +0300

    show_cart

--- Routes/CartRoutes.js
+++ Routes/CartRoutes.js
@@ -3,13 +3,13 @@ const cartController = require("./../Controllers/CartController");
 const authController = require("./../Controllers/AuthController");
 const router = express.Router();
 
-router.route("/").post(authController.protect, cartController.AddToCart);
+router.route("/").post( authController.protect,cartController.AddToCart);
 router
   .route("/:id/clear")
   .post(authController.protect, cartController.clearCart);
 
 router.route("/:id/count").get(cartController.getTotalProductsInCart);
 
-router.route("/showcart").get(cartController.showCart);
+router.route("/:id/showcart").get(cartController.showCart);
 
 module.exports = router;
