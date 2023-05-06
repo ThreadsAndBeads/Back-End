eea8670ac676f18559bae07e21284d5cbb11107d
marim99 <marim.khaled99@gmail.com>
2023-05-07 00:39:57 +0300

    create workshop

--- Controllers/WorkshopController.js
+++ Controllers/WorkshopController.js
@@ -4,13 +4,14 @@ const AppError = require("./../utils/appError");
 
 exports.createWorkshop = async (req, res, next) => {
   try {
-    console.log(req.params.seller_id);
-    const seller_id = await User.findById(req.params.seller_id);
-    if (!seller_id) {
+    // console.log(req.body.seller_id);
+    let seller_id = req.body.seller_id;
+    const seller = await User.findById(seller_id);
+    if (!seller) {
       return next(new AppError("Seller id not found in users", 404));
     }
     const newWorkshop = await Workshop.create({
-      seller_id: req.body.seller_id,
+      seller_id: seller_id,
       title: req.body.title,
       category: req.body.category,
       price: req.body.price,
