189997bb91737e79a4e693762d6cbe59e738256b
Radwa Khalil <95037451+radwakhalil22@users.noreply.github.com>
2023-05-24 08:50:50 +0300

    server

---
+++ server.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+const app = require("./app");
+const DB_URL = process.env.DATABASE_URL;
+const DB = process.env.DATABASE;
+
+mongoose
+    .connect(DB, {
+    useNewUrlParser: true,
+    })
+    .then(() => {
+    console.log("DB connection successful");
+    })
+    .catch((err) => {
+    // console.error("Connection error:", err);
+    });
+
+const server = app.listen(process.env.PORT, () => {
+    console.log("http://localhost:" + process.env.PORT);
+});
